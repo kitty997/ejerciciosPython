da965eaef76ed08cbaa68e41c1bef61c322f9435
kitty997 <aymara.perez@unisa.edu.mx>
2025-08-05 18:10:07 -0600

    Ejercicio némeros flotantes 1

---
+++ numerosflotantes1.py
@@ -0,0 +1,8 @@
+precio = 288.50
+descuento = precio * 0.10
+print(descuento)
+
+precio_con_descuento = precio - descuento
+
+print("El nuevo precio es:",precio_con_descuento)
+print(type(precio_con_descuento))
