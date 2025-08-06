8fa43c396f10c9b519d41d289fe77468c27d37a3
kitty997 <aymara.perez@unisa.edu.mx>
2025-08-05 18:22:06 -0600

    Ejercicio números flotantes2

---
+++ numerosflotantes.py
@@ -0,0 +1,11 @@
+calificacion_actual = 93.56
+calificacion_maxima = 100.0
+
+porcentaje = (calificacion_actual / calificacion_maxima) * 100
+
+print("Tu porcentaje es:", porcentaje)
+
+print("El tipo de dato de porcentaje es:", type(porcentaje))
+
+
+
