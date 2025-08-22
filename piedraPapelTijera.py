e0400f1aeb0161e9ed66ce20c462fdae369fb80a
kitty997 <aymara.perez@unisa.edu.mx>
2025-08-21 18:46:29 -0600

    Mini reto 2

--- piedraPapelTijera.py
+++ piedraPapelTijera.py
@@ -1,5 +1,9 @@
-judador_1 = "piedra"
-jugador_2 = "pidra"
+print("=" * 50)
+print("Bienvenidos al juega piedra, papel o tijera")
+print("=" * 50)
+judador_1 = (input("¿Qué eliges: piedra, papel o tijera? "))
+jugador_2 = (input("¿Qué eliges: piedra, papel o tijera? "))
 
 print("El jugador uno selecciono",judador_1)
 print("El jugador dos selecciono",jugador_2)
+
